f4ab15d1e3653661cd93e6fc60e57064750528d4
Ihor849 <Marchelo849@outlook.com>
2023-07-12 18:22:26 +0300

    1

---
+++ app.js
@@ -0,0 +1,8 @@
+//ptQvw*8PdgWQsZH
+
+const mongoose =require("mongoose");
+
+const DB_HOST="mongodb+srv://Ihor:ptQvw*8PdgWQsZH@cluster0.qbxqqlz.mongodb.net/contacts_friends?retryWrites=true&w=majority"
+mongoose.connect(DB_HOST)
+.then(()=>console.log("Database connect success"))
+.catch(error=>console.log(error.message));
